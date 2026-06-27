8bfa2bd45275457f617a23274a8c5f686883d1cf
Fiol <fidyt.daniell@gmail.com>
2026-06-28 01:12:44 +0200

    add small changes to silver layer

--- dbt/olist_ecommerce/models/silver/order_reviews_silver.py
+++ dbt/olist_ecommerce/models/silver/order_reviews_silver.py
@@ -5,12 +5,12 @@ def model(dbt,session):
 
     bucket_path = "gs://olist_ecomerce_bucket/raw_data/olist_order_reviews_dataset.csv"
 
-    bronze_df = session.read.format('csv').option('header','true').option('inferSchema','true').load(bucket_path)
+    __schema = 'review_id string,order_id string,review_score int, review_comment_title string,review_comment_message string,review_creation_date timestamp,review_answer_timestamp timestamp'
+    bronze_df = session.read.format('csv').schema(__schema).option('header','true').option('multiLine','true').load(bucket_path)
 
     silver_df = bronze_df.filter(
         (F.col('review_score').between(1,5)) &
-        (F.col('review_creation_date').isNotNull()) &
-        (F.col('review_answer_timestamp').isNotNull())
+        (F.col('review_creation_date').isNotNull())
     )
 
     return silver_df
--- dbt/olist_ecommerce/models/silver/orders_silver.py
+++ dbt/olist_ecommerce/models/silver/orders_silver.py
@@ -8,7 +8,7 @@ def model(dbt,session):
     bronze_df = session.read.format('csv').option('header','true').option('inferSchema','true').load(bucket_path)
 
     silver_df = bronze_df.withColumn(
-        F.col('order_status'), F.when(F.col('order_status')=='canceled','cancelled') \
+        'order_status', F.when(F.col('order_status')=='canceled','cancelled') \
         .otherwise(F.col('order_status'))
     )
 
